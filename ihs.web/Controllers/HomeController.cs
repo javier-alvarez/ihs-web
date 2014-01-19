d23e8986e26803a7c36bc971c9fb7a48cdaefb01
Javier Alvarez-Valle <javierav@outlook.com>
2014-01-19 22:29:17 +0000

    - Add bootstrap - Show temperature graph

--- ihs.web/Controllers/HomeController.cs
+++ ihs.web/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace ihs.web.Controllers
             string fullname =
                    string.Format("{0} {1}", cp.FindFirst(ClaimTypes.GivenName).Value,
                    cp.FindFirst(ClaimTypes.Surname).Value);
-            ViewBag.Message = string.Format("Dear {0}, welcome to the Expense Note App",
+            ViewBag.Message = string.Format("Dear {0}, welcome!",
                               fullname);
 
 
